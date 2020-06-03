04284ea4188588fc8d358038d79e2b2739cdc95a
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-06-04 01:34:54 +0200

    chore: add NewLine stmt & work on profiles loader

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Require Third-party Dependencies
 const astring = require("astring");
-const { Program } = require("node-estree");
+const { Program, VariableDeclaration, Identifier, Helpers } = require("node-estree");
 const oop = require("@slimio/oop");
 
 // Require Internal Dependencies
@@ -14,23 +14,30 @@ const {
     registerCallback,
     scheduleCallback,
     addonLockOn,
-    addonIsReady
+    addonIsReady,
+    onEvent
 } = require("./src/presets");
 
 class AddonFactory {
+    #hasConfigPackage = false;
+
     constructor(addonName, options = Object.create(null)) {
         this.addonName = oop.toString(addonName);
-        this.options = {
-            verbose: oop.toNullableBoolean(options.verbose),
-            description: oop.toNullableString(options.description)
+        this.addonOptions = {
+            verbose: Boolean(options.verbose),
+            description: oop.toNullableString(options.description) ?? ""
         };
+
         this.callbacks = new Map();
         this.scheduled = [];
         this.locks = [];
+        this.importProfiles = Boolean(options.importProfiles);
     }
 
     lockOn(addonName) {
         this.locks.push(addonLockOn(this.addonName, addonName));
+
+        return this;
     }
 
     registerCallback(name, body = []) {
@@ -45,6 +52,8 @@ class AddonFactory {
         }
 
         this.scheduled.push(scheduleCallback(this.addonName, callbackName, options));
+
+        return this;
     }
 
     generateCode() {
@@ -53,12 +62,34 @@ class AddonFactory {
         if (this.scheduled.length > 0) {
             AST.add(importPackage("Scheduler", "@slimio/scheduler"));
         }
-        AST.add(createAddon(this.addonName, this.options));
+        if (this.importProfiles) {
+            AST.add(importPackage("Config", "@slimio/config"));
+            AST.add(importPackage("profilesLoader", "@slimio/profiles"));
+            this.#hasConfigPackage = true;
+        }
+
+        AST.add({ type: "NewLine" });
+        if (this.importProfiles) {
+            // TODO: implement void declaration in node-estree
+            // console.log(VariableDeclaration.createOne("let", "profiles").toJSON());
+            // AST.add();
+        }
+        AST.add(createAddon(this.addonName, this.addonOptions));
         for (const node of this.locks) {
             AST.add(node);
         }
-        AST.add(addonIsReady(this.addonName, this.locks.length > 0 ? "awake" : "start"));
 
+        AST.add({ type: "NewLine" });
+        AST.add(addonIsReady(this.addonName, this.locks.length > 0 ? "awake" : "start"));
+        AST.add({ type: "NewLine" });
+        if (this.importProfiles) {
+            const node = onEvent(this.addonName, "stop", {
+                body: [Helpers.FastCall(null, ["profiles", "free"])()]
+            });
+            AST.add(node);
+        }
+
+        AST.add({ type: "NewLine" });
         for (const [callbackName, node] of this.callbacks.entries()) {
             AST.add(node);
             AST.add(registerCallback(this.addonName, callbackName));
@@ -68,13 +99,22 @@ class AddonFactory {
             AST.add(node);
         }
 
+        AST.add({ type: "NewLine" });
         AST.add(exportAddon(this.addonName));
 
-        return astring.generate(AST.toJSON(), { comments: true });
+        return astring.generate(AST.toJSON(), {
+            comments: true,
+            generator: {
+                ...astring.baseGenerator,
+                NewLine(node, state) {
+                    state.write("");
+                }
+            }
+        });
     }
 }
 
-const addon = new AddonFactory("cpu");
+const addon = new AddonFactory("cpu", { importProfiles: true });
 addon.lockOn("events");
 addon.registerCallback("SayHello");
 addon.scheduleCallback("SayHello");
